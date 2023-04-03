3b446f494a29d58fc609f5ba846c15b6a12f06b7
mikevanis <hello@mikevanis.com>
2023-04-03 22:16:33 +0100

    add port env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const fs = require('fs')
 const isImage = require('is-image')
 
