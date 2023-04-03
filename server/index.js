8ddc38a62e9af27cff61956eeb1087a4e1e9e0ae
mikevanis <hello@mikevanis.com>
2023-04-03 17:44:50 +0100

    serve list of images when requesting /uploads

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const app = express();
-const port = 3000;
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+const port = 3000
+const fs = require('fs')
+const isImage = require('is-image')
 
 app.use(
     fileUpload({
@@ -17,6 +19,22 @@ app.use(express.static('./public'))
 
 // Serve images
 app.use('/uploads', express.static('./upload'))
+
+// Serve list of images
+app.get('/uploads', (req, res) => {
+    fs.readdir(__dirname + '/upload', (err, files) => {
+        if (err) {
+            return res.sendStatus(500)
+        }
+        let images = []
+        files.forEach(file => {
+            if (isImage(file)) {
+                images.push(file)
+            }
+        })
+        return res.json(images)
+    })
+})
 
 // Upload route
 app.post('/upload', (req, res) => {
